841e29d475f2777dce45ac6fe8d0526042298132
Serkan Korkmaz <77464572+serkor1@users.noreply.github.com>
2023-10-01 09:19:53 +0200

    Updated packages imports

--- R/cryptoQuotes-package.R
+++ R/cryptoQuotes-package.R
@@ -2,5 +2,13 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom curl has_internet
+#' @importFrom httr content
+#' @importFrom httr GET
+#' @importFrom jsonlite fromJSON
+#' @importFrom rlang abort
+#' @importFrom rlang trace_back
+#' @importFrom xts as.xts
+#' @importFrom zoo as.zoo
 ## usethis namespace: end
 NULL
