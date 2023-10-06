00e5d00f6a98f16704afdeca2e97833a40b37682
serkor1 <77464572+serkor1@users.noreply.github.com>
2023-10-06 17:51:26 +0200

    Readded TRY
    To all examples using GET

--- man/examples/scr_availableTickers.R
+++ man/examples/scr_availableTickers.R
@@ -1,21 +1,24 @@
-\dontrun{
-  ## available tickers
-  ## in Binance spot market
-  head(
+## available tickers
+## in Binance spot market
+head(
+  try(
     cryptoQuotes::availableTickers(
       source = 'binance',
       futures = FALSE
     )
   )
+)
 
 
-  ## available tickers
-  ## in Kraken futures market
-  head(
+## available tickers
+## in Kraken futures market
+head(
+  try(
     cryptoQuotes::availableTickers(
       source = 'kraken',
       futures = TRUE
     )
   )
-}
+)
+
 
--- man/examples/scr_getQuote.R
+++ man/examples/scr_getQuote.R
@@ -1,21 +1,27 @@
-\dontrun{
-  # 1) load perpetual
-  # futures
+# 1) load perpetual
+# futures
 
-  perpAtom <- cryptoQuotes::getQuote(
+perpAtom <- try(
+  cryptoQuotes::getQuote(
     ticker  = 'ATOMUSDT',
     source = 'binance',
     interval = '15m',
     futures = TRUE
   )
+)
 
-  # 2) chart the futures
+# 2) chart the futures
+# using the chart
+# function
+if (!inherits(perpAtom, 'try-error')){
   cryptoQuotes::chart(
     perpAtom
   )
-
 }
 
+# NOTE: Without the try
+# the examples fails on
+# Github Actions
 
 
 # script end;
