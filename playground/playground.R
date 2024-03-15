df5c565b10c9909281239192e20cc24bb8d6d114
Serkan Korkmaz <77464572+serkor1@users.noreply.github.com>
2024-03-15 06:07:28 +0100

    CRAN Release (#8)
    Created release for version 1.3.0.
    
    Added playground to .gitignore.

--- playground/playground.R
+++ playground/playground.R
@@ -9,13 +9,45 @@
 # setup;
 rm(list = ls()); gc(); devtools::load_all()
 
-DOGE <- cryptoQuotes::get_quote(
-  ticker   = 'DOGEUSDTM',
-  interval = '1m',
-  source   = 'kucoin',
-  futures  = TRUE
+BTC <- cryptoQuotes::get_quote(
+  ticker   = 'BTCUSD',
+  interval = '1d',
+  source   = 'kraken',
+  futures  = FALSE,
+  from     = Sys.Date() - 1000,
+  to       = Sys.Date()
+)
+
+nrow(BTC)
+
+
+head(BTC)
+head(BTC)
+
+
+
+length(
+  seq(
+    from = coerce_date(Sys.Date() - 10),
+    to   = coerce_date(Sys.Date() - 1),
+    by   = "+15 mins"
+  )
 )
 
 
+default_dates(
+  interval = "15m",
+  from = coerce_date(Sys.Date() - 10),
+  to   = coerce_date(Sys.Date() - 1)
+)
 
 # script end;
+
+
+origin_date <- '1970-01-01'
+
+as.POSIXct(
+  trunc(as.double(coerce_date(Sys.Date() - 10))/(15*60))*(15*60),
+  tz = 'UTC',
+  origin = origin_date
+)
