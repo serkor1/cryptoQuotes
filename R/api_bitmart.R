d86f32ef25c72768aa60edb39c05654337e3d9f9
Serkan Korkmaz <77464572+serkor1@users.noreply.github.com>
2024-11-01 07:16:01 +0100

    BitMart update :fire: (#38)
    The BitMart exchange have updated their futures API to V2. V1 will stop
    working 11/30/2024.

--- R/api_bitmart.R
+++ R/api_bitmart.R
@@ -12,7 +12,7 @@ bitmartUrl <- function(
   # 1) define baseURL
   # for each API
   if (futures)
-    'https://api-cloud.bitmart.com'
+    'https://api-cloud-v2.bitmart.com'
   else
     'https://api-cloud.bitmart.com'
 
@@ -87,7 +87,7 @@ bitmartIntervals <- function(
 
     interval_label <- c(
       '1m',
-      # '3m',
+      '3m',
       '5m',
       '15m',
       '30m',
@@ -103,7 +103,7 @@ bitmartIntervals <- function(
 
     interval_actual <- c(
       1,
-      # 3,
+      3,
       5,
       15,
       30,
